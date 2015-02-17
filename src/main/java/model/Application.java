e9d2b3b12447dc6119812b1bf7dfbb554f5c10a7
Adriansun <adriansun299@gmail.com>
2015-02-17 15:08:35 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/model/Job.java
    	src/main/java/model/Job_Localized.java

--- src/main/java/model/Application.java
+++ src/main/java/model/Application.java
@@ -14,6 +14,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 
+/**
+ * Klassen Applikation. Applikationer skapas i denna klass.
+ * @author Ansoa
+ */
 @Entity
 public class Application implements Serializable {
     @Id
@@ -48,9 +52,19 @@ public class Application implements Serializable {
    // @JoinColumn(name = "job", referencedColumnName = "id", nullable = false)
     //private Job job;
     
+    /**
+     * Tom konstruktor.
+     */
     public Application(){
     }
     
+    /**
+     * Inparametrar då en applikation görs-
+     * @param date_made Datumet applikationen gjordes.
+     * @param person Vem som gjorde applikationen.
+     * @param status Statusen på om applikationen blivit recenserad av rekryterare.
+     * @param job Vilken tjänst / typ av job.
+     */
     public Application(String date_made, Person person, Status status, Job job)
     {
         this.date_made = date_made;
@@ -59,38 +73,76 @@ public class Application implements Serializable {
         this.job = job;
     }
     
+    
+    /**
+     * Returnerar ett nummer av kolumnen ID i en applikation från en sökande.
+     * @return Raden ids nummer.
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Returnerar datumet då applikationen från en sökande gjordes.
+     * @return skapande datum.
+     */
     public String getDateMade(){
         return date_made;
-    }
+    } 
     
+    /**
+     * Skriver in datumet i databasen då applikationen gjordes.
+     * @param date_made dåvarande datum
+     */
     public void setDateMade(String date_made){
         this.date_made = date_made;
     }
     
+    /**
+     * Returnerar användarens ID av den som gjorde applikationen.
+     * @return personens id
+     */
     public Integer getPerson(){
         return person.getId();
     }
     
+    /**
+     * Skriver in personen i databasen som gjorde applikationen.
+     * @param person själva personen
+     */
     public void setPerson(Person person){
         this.person = person;
     }
     
+    /**
+     * Berättar om applikationen blivit godkänd eller inte.
+     * @return id:ets status 
+     */
     public Integer getStatus(){
         return status.getId();
     }
     
+    /**
+     * Skriver in status på applikationen i databasen om applikationen
+     * eller recenserad eller ej.
+     * @param status på id:et.
+     */
     public void setStatus(Status status){
         this.status = status;
     }
     
+    /**
+     * Returnerar ett ID från databasen om ett specifikt jobb.
+     * @return ID om jobb
+     */
     public Integer getJob(){
         return job.getId();
     }
     
+    /**
+     * Skriver in i databasen om en specifikt jobbtjänst.
+     * @param job Tjänst
+     */
     public void setJob(Job job){
         this.job = job;
     }
